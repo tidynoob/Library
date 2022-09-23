ffe8735dfe8d6be1c3a680f84c23c914f622651a
tidynoob <mitchellgriffin9@gmail.com>
2022-09-23 02:30:14 -0400

    Add form backdrop

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 let myLibrary = [];
 
-function Book (title, author, pages, status) {
+function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.status = status;
 }
 
-
-let addBookButton = document.querySelector('#add-book');
-let submitBookButton = document.querySelector('#submit-book')
-
 let openForm = (e) => {
-    let form = document.querySelector('.form-window');
-    form.style.display='block';
+    // let form = document.querySelector('.form-window');
+    formWindow.style.display = 'block';
+    formBackdrop.style.display = 'block';
 };
 
 let closeForm = (e) => {
-    let form = document.querySelector('.form-window');
-    form.style.display='none';
+    // let form = document.querySelector('.form-window');
+    formWindow.style.display = 'none';
+    formBackdrop.style.display = 'none';
 };
 
 let addBook = (e) => {
     console.log(e);
 }
 
+let addBookButton = document.querySelector('#add-book');
+let submitBookButton = document.querySelector('#submit-book');
+let formWindow = document.querySelector('.form-window');
+let formBackdrop = document.querySelector('.form-backdrop');
+
 addBookButton.addEventListener('click', openForm);
 
-submitBookButton.addEventListener('click', (e) => {
-    console.log(e);
-    addBook(e);
-    closeForm(e);
+window.addEventListener('click', (e) => {
+    // console.log(e.target);
+    console.log(formWindow.style.display);
+    let clickInside = formWindow.contains(e.target) || addBookButton.contains(e.target);
+    if (!clickInside) closeForm()
 });
+
+// submitBookButton.addEventListener('click', (e) => {
+//     console.log(e);
+//     addBook(e);
+//     closeForm(e);
+// });
